fc749a303af8ce6ee0496bf3e6703ba1b45e0e48
JacquelineParedes <30506026+JacquelineParedes@users.noreply.github.com>
2022-03-01 02:49:33 -0600

    Usuario
    Base de datos y api usuario

---
+++ Brive.Bootcamp.DatosProyecto/Brive.Bootcamp.DatosProyecto/dbo/Tables/Busqueda.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Busqueda]
+(
+	[Id_Busqueda] INT IDENTITY (1, 1) NOT NULL,
+	[NombreEmpresa] VARCHAR(50) NOT NULL,
+	[Total_Vacantes] INT NOT NULL,
+	[FechaBusqueda] DATETIME     DEFAULT (getdate()) NOT NULL,
+	[Id_Busqueda_Usuario] INT NOT NULL
+	CONSTRAINT [PK_Busqueda] PRIMARY KEY CLUSTERED ([Id_Busqueda] ASC)
+)
---
+++ Brive.Bootcamp.DatosProyecto/Brive.Bootcamp.DatosProyecto/dbo/Tables/Busqueda_usuario.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Busqueda_Usuario] (
+    [Id_Busqueda_Usuario]              INT          IDENTITY (1, 1) NOT NULL,
+    [IdUsuario]         INT NOT NULL,
+    [IdBusqueda]        INT NOT NULL
+    CONSTRAINT [PK_busqueda_usuario] PRIMARY KEY CLUSTERED ([Id_Busqueda_Usuario] ASC)
+);
