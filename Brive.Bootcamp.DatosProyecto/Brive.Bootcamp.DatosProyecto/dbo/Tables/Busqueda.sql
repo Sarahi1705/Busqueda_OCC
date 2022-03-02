fc2e175a3a6450d5934af029b8dcdcb24f878691
JacquelineParedes <30506026+JacquelineParedes@users.noreply.github.com>
2022-03-02 00:01:05 -0600

    intento API

--- Brive.Bootcamp.DatosProyecto/Brive.Bootcamp.DatosProyecto/dbo/Tables/Busqueda.sql
+++ Brive.Bootcamp.DatosProyecto/Brive.Bootcamp.DatosProyecto/dbo/Tables/Busqueda.sql
@@ -4,6 +4,7 @@
 	[NombreEmpresa] VARCHAR(50) NOT NULL,
 	[Total_Vacantes] INT NOT NULL,
 	[FechaBusqueda] DATETIME     DEFAULT (getdate()) NOT NULL,
-	[Id_Busqueda_Usuario] INT NOT NULL
-	CONSTRAINT [PK_Busqueda] PRIMARY KEY CLUSTERED ([Id_Busqueda] ASC)
+	[Id_Busqueda_Usuario] INT NOT NULL,
+	PRIMARY KEY ([Id_Busqueda]),
+	CONSTRAINT [FK_Busqueda] FOREIGN KEY ([(Id_Usuario)]) REFERENCES ([Usuario][[Id_Usuario]])
 )
